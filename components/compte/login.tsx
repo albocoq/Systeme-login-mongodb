854461f372065a5393478845f20a5565a3b10cd7
albocoq <118123935+albocoq@users.noreply.github.com>
2023-07-07 14:28:48 +0200

    Update login.tsx

--- components/compte/login.tsx
+++ components/compte/login.tsx
@@ -17,16 +17,6 @@ export default function Sign() {
                     <h5>Log into your account</h5>
                     <div className={styles.container}>
                         <Inputs />
-                        <div className={styles.social}>
-                            <button
-                                onClick={() => signIn('google', { callbackUrl: "/" })}
-                            >
-                                <Image src="/images/google-logo.png" alt="" width={25} height={40}/>
-                            </button>
-                            <button>
-                                <Image src="/images/logo-apple.png" alt="" width={30} height={40}/>
-                            </button>
-                        </div>    
                         <p className={styles.login}>
                             Don&apos;t have a account ? <a href={"./sign"}>Sign</a>
                         </p>                        
